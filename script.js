b031932c63355187c017688bc6b46164d06e76a9
Kofi Koranteng <102839563+Kofikay32@users.noreply.github.com>
2023-10-18 02:31:01 -0700

    changes to about us page

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ if (close) {
       nav.classList.remove('active');
     })
 }
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
